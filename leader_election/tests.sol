e84a827aa975b840f9a9b70a00bd3005f2da1f02
Jonas <50956512+jonas-ba@users.noreply.github.com>
2023-10-26 03:47:16 +0200

    .

--- leader_election/tests.sol
+++ leader_election/tests.sol
@@ -6,18 +6,15 @@ import "./leader_election.sol";
 contract Test {
     using CreateTournament for Tournament;
     using RunTournament for Tournament;
-    using RndSubmission for Tournament;
 
     Tournament public tournament;
 
     // create tree with n players with equal shares
-    function createTournament(uint playerCount) private {
+    function createTournament(uint playerCount) public {
         for (uint i = 1; i <= playerCount; i++) {
             tournament.addPlayer(player(i));
             tournament.addWeight(player(i), 1);
         }
-        tournament.setTotalRounds();
-        console.log("Running checkWinningProposal");
     }
 
     function player(uint number) private pure returns(address) {
@@ -26,12 +23,17 @@ contract Test {
 
     // Test with 2 players
     function test1() public {
+        console.log("step 1");
         createTournament(2);
+        console.log("step 2");
         tournament.compete(player(1), 0);
+        console.log("step 3");
         tournament.compete(player(2), 0);
+        console.log("step 4");
         tournament.currentRound++;
+        console.log("step 5");
         address winner = tournament.getTournamentWinner();
-        emit PlayerLog("winner and total weight: ", winner, tournament.weights[winner]);
+        console.log("step 6");
     }
     
 }
